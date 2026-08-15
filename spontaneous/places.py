82cecdb0ba1d43ad125f3f981fea9c9b7dd29e1b
bong <xnddl456@gmail.com>
2026-08-16 00:02:09 +0900

    add: add type

--- spontaneous/places.py
+++ spontaneous/places.py
@@ -267,3 +267,24 @@ def convert_to_course_place(
         "themes": themes,
         "raw": place,
     }
+
+COURSE_CONTENT_TYPES = {
+    "12", # 관광지
+    "14", # 문화시설
+    "15", # 축제공연
+    "28", # 레포츠
+    "38", # 쇼핑
+    "39", # 음식
+}
+
+
+def filter_course_candidates(
+    places: list[dict],
+) -> list[dict]:
+    
+    return [
+        place
+        for place in places
+        if str(place.get("contenttypeid"))
+        in COURSE_CONTENT_TYPES
+    ]
